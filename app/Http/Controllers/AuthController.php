5fc5f96c99113dde92ef2b5611d6b47fdffeb7f4
unknown <111202214137@mhs.dinus.ac.id>
2026-01-04 04:33:36 +0800

    update1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,12 +24,37 @@ class AuthController extends Controller
                     # code...
                 } elseif ($user->role == 'dokter') {
                     return redirect()->route('dokter.dashboard');
-                } elseif ($user->role == 'pasien'){
+                } else{
                     return redirect()->route('pasien.dashboard');
                 } 
 
             }
             return back()->withErrors(['email'=> 'Email atau password Salah !']);        
         }
-            
+
+    public function register (Request $request)
+    {
+        $request->validate([
+            'nama' =>['required','string','max:255'],
+            'alamat' => ['required','string','max:255'],
+            'no_ktp' => ['required','string','max:30'],
+            'no_hp' => ['required','string','max:20'],
+            'email'=> ['required','string','email', 'max:255', 'unique:user,email'],
+            'password'=> ['required','confirmed'],
+        ]);
+
+        User::create([
+            'nama' => $request->nama,
+            'alamat'=> $request->alamat,
+            'no_ktp'=> $request->no_ktp,
+            'no_hp'=> $request->no_hp,
+            'email'=> $request->email,
+            'password' => Hash::$request->password,
+            'role' => 'pasien',
+        ]);
+        
+
+        return redirect()->route('login');
+    }
 }
+
